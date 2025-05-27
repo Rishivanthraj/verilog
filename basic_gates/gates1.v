ee337b6f5d4d1aa240e75a706eb965afa4230521
Rishivanthraj <rishivanthraj.k@gmail.com>
2025-05-27 16:09:00 +0530

    Update gates1.v

--- basic_gates/gates1.v
+++ basic_gates/gates1.v
@@ -1,1 +1,15 @@
-module 
+module gates(
+input a,b
+output g_and,g_or,g_nor,g_nand,g_xor,g_xnor,g_not_a,g_not_b
+);
+
+  assign g_and  = a & b;
+  assign g_or   = a | b;
+  assign g_nand = ~(a & b);
+  assign g_nor  = ~(a | b);
+  assign g_xor  = a ^ b;
+  assign g_xnor = ~(a ^ b);
+  assign g_not_a = ~a;
+  assign g_not_b = ~b;
+
+endmodule
